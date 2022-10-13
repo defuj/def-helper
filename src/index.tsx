855cf20213bdf24f6fe63350051323ecd7a848e0
defuj <defuj.official@gmail.com>
2022-10-13 14:44:58 +0700

    add new function

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
-export const slugify = (str : String) : String =>
-  str
+export const slugify = (str : String) : String => str
     .toLowerCase()
     .trim()
     .replace(/[^\w\s-]/g, '')
@@ -19,4 +18,14 @@ export const validatePhone = (phone : String) : any => {
 
 export const getNumberInFirstString = (str : String) : String => {
   return str.replace(/(^\d+)(.+$)/i,'$1');
+}
+
+export const validateURL = (str : string) => {
+  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return !!pattern.test(str);
 }
