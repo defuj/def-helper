93b9245cbe16ecb22a1bb6a1d24c059577343e3b
Dede Fuji Abdul <defuj.official@gmail.com>
2022-10-13 09:28:14 +0700

    Delete dist directory

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const slugify: (str: String) => String;
-export declare const validateEmail: (email: String) => RegExpMatchArray | null;
-export declare const validatePhone: (phone: String) => any;
